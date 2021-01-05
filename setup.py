c17df5f5680889207ec8601186abcab227f789e6
Evgeniy Byrdin <e.s.burdin@yandex.ru>
2021-01-05 10:17:42 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from os.path import dirname, join
+
+from setuptools import setup
+
+setup(
+    name='valdec',
+    version='0.9.0',
+    license='MIT',
+    author='Evgeniy Burdin',
+    author_email='e.s.burdin@mail.ru',
+    py_modules=['valdec'],
+    description='Decorator for validating function arguments and result.',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/EvgeniyBurdin/valdec',
+    keywords='validation function',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ],
+    python_requires='>=3.7',
+)
