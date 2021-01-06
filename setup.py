dbecb66f3eab8fd319aef014dec79aad4868d693
Evgeniy Byrdin <e.s.burdin@yandex.ru>
2021-01-06 11:10:46 +0400

    version='1.0.1'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='valdec',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='Evgeniy Burdin',
     author_email='e.s.burdin@mail.ru',
