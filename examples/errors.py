00c1bff86c11bd85e3ad0b8c3a5641f5b851e2c0
Evgeniy Byrdin <e.s.burdin@yandex.ru>
2021-01-06 15:21:51 +0400

    Добавил пример с ошибками

---
+++ examples/errors.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pydantic import StrictInt, StrictStr
+from valdec.decorators import validate
+from valdec.utils import ValidationArgumentsError, ValidationReturnError
+
+
+class Foo:
+
+    @validate  # all arguments with annotations and return
+    def bar_1(self, i: StrictInt, s: Optional[StrictStr] = None):
+        pass
+
+    @validate("return")  # only "return"
+    def bar_2(self, i: StrictInt, s: Optional[StrictStr] = None):
+        return i
+
+
+foo = Foo()
+
+foo.bar_1(1, "2")  # ok
+
+try:
+    foo.bar_1(1, 2)
+except ValidationArgumentsError as error:
+    assert "s" in str(error)
+
+
+foo.bar_2(None, 1)  # ok
+
+try:
+    foo.bar_2(1, 2)
+except ValidationReturnError as error:
+    assert "result" in str(error)
