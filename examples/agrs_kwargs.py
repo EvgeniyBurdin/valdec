432c00a5e7a1883c56c7659474e7598ddf0cac7b
Evgeniy Byrdin <e.s.burdin@yandex.ru>
2021-01-10 17:38:55 +0400

    Добавил пример для функции с сигнатурой содержащей *args и **kwargs

---
+++ examples/agrs_kwargs.py
@@ -0,0 +1,39 @@
+from valdec.data_classes import Settings
+from valdec.decorators import validate as _validate
+from valdec.utils import ValidationArgumentsError
+from valdec.validators import validated_dc_validator
+
+custom_settings = Settings(
+    validator=validated_dc_validator,  # function for validation
+)
+
+
+def validate(*args, **kwargs):  # define new decorator
+    kwargs["settings"] = custom_settings
+    return _validate(*args, **kwargs)
+
+
+@validate
+def func(a1: int, a2: str, *args, k1: str, k2: int = 1,  **kwargs):
+    pass
+
+
+func(1, "2", 3, 4, 5, k1="6", k2=7, k3="any")  # ok
+
+
+try:
+    func(1, 2, 3, 4, 5, k1="6", k2=7, k3="any")  # a2 not str
+except ValidationArgumentsError as error:
+    assert "a2" in str(error)
+
+
+try:
+    func(1, "2", 3, 4, 5, k1=6, k2=7, k3="any")  # k1 not str
+except ValidationArgumentsError as error:
+    assert "k1" in str(error)
+
+
+try:
+    func(1, "2", 3, 4, 5, k1="6", k2="7", k3="any")  # k2 not int
+except ValidationArgumentsError as error:
+    assert "k2" in str(error)
